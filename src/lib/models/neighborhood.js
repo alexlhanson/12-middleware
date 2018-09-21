a04c3a69977e7cf19c4e5814a731e659be0f4368
alexlhanson <alexlhanson@gmail.com>
2018-09-21 15:39:00 -0700

    setup constructor for neighborhoods

--- src/lib/models/neighborhood.js
+++ src/lib/models/neighborhood.js
@@ -1,1 +1,24 @@
 'use strict';
+//debugger
+const debug = require('debug')('neighborhood');
+
+import storage from '../storage/data-store';
+import uuid from 'uuid';
+
+class Neighborhood{
+  
+  constructor(config){
+    this.id = uuid();
+    this.createdOn = new Date();
+    this.name = config && config.name || '';
+    this.population = config && config.population || '';
+    this.use = config && config.use;
+  }
+
+  save(){
+    return storage.save(this);
+  }
+
+}
+
+export default Neighborhood;
